a28e63f2705ab0dbae46a89d7dfcb0eec33c81f1
Alex Noir <Varsash@Gmail.com>
2015-09-12 20:44:44 +0300

    MicroUZI finalized, balancing tweaks to Thompson and UZI, clip now invisible on gun when it's not inserted

--- magazines.cs
+++ magazines.cs
@@ -1,221 +1,235 @@
 function Player::takeMagazineProps(%this, %item)
 {
-    %index = %this.findMagazine(%item);
+	%index = %this.findMagazine(%item);
 
-    if (%index < 0)
-        return 0;
+	if (%index < 0)
+		return 0;
 
-    %props = %this.itemProps[%index];
+	%props = %this.itemProps[%index];
 
-    %this.itemProps[%index] = "";
-    %this.removeTool(%index);
+	%this.itemProps[%index] = "";
+	%this.removeTool(%index);
 
-    return %props;
+	return %props;
 }
 
 function Player::giveMagazineProps(%this, %props)
 {
-    if (isObject(%this.client) && %props.count < 1)
-    {
-        if (%this.client.discardEmptyMagazine == 2)
-        {
-            %props.delete();
-            return;
-        }
+	if (isObject(%this.client) && %props.count < 1)
+	{
+		if (%this.client.discardEmptyMagazine == 2)
+		{
+			%props.delete();
+			return;
+		}
 
-        if (%this.client.discardEmptyMagazine == 1 && %this.findMagazine(%props.weapon) >= 0)
-        {
-            %props.delete();
-            return;
-        }
-    }
+		if (%this.client.discardEmptyMagazine == 1 && %this.findMagazine(%props.weapon) >= 0)
+		{
+			%props.delete();
+			return;
+		}
+	}
 
-    %maxTools = %this.getDataBlock().maxTools;
+	%maxTools = %this.getDataBlock().maxTools;
 
-    for (%i = 0; %i < %maxTools; %i++)
-    {
-        if (!isObject(%this.tool[%i]))
-            break;
-    }
+	for (%i = 0; %i < %maxTools; %i++)
+	{
+		if (!isObject(%this.tool[%i]))
+			break;
+	}
 
-    if (%i == %maxTools)
-    {
-        // todo: drop mag
-        %props.delete();
-        return;
-    }
+	if (%i == %maxTools)
+	{
+		// todo: drop mag
+		%props.delete();
+		return;
+	}
 
-    %this.tool[%i] = %props.sourceItemData.getID();
-    %this.itemProps[%i] = %props;
+	%this.tool[%i] = %props.sourceItemData.getID();
+	%this.itemProps[%i] = %props;
 
-    if (isObject(%this.client))
-        messageClient(%this.client, 'MsgItemPickup', '', %i, %props.sourceItemData.getID(), 1);
+	if (isObject(%this.client))
+		messageClient(%this.client, 'MsgItemPickup', '', %i, %props.sourceItemData.getID(), 1);
 }
 
 function Player::findMagazine(%this, %item)
 {
-    %item = %item.getID();
-    %maxTools = %this.getDataBlock().maxTools;
+	%item = %item.getID();
+	%maxTools = %this.getDataBlock().maxTools;
 
-    %bestSlot = -1;
-    %bestCount = -1;
+	%bestSlot = -1;
+	%bestCount = -1;
 
-    for (%i = 0; %i < %maxTools; %i++)
-    {
-        %props = %this.getItemProps(%i);
+	for (%i = 0; %i < %maxTools; %i++)
+	{
+		%props = %this.getItemProps(%i);
 
-        if (isObject(%props.weapon) && %props.weapon.getID() == %item)
-        {
-            if (%props.count > %bestCount)
-            {
-                %bestSlot = %i;
-                %bestCount = %props.count;
-            }
-            else if (%bestSlot == -1)
-                %bestSlot = -2;
-        }
-    }
+		if (isObject(%props.weapon) && %props.weapon.getID() == %item)
+		{
+			if (%props.count > %bestCount)
+			{
+				%bestSlot = %i;
+				%bestCount = %props.count;
+			}
+			else if (%bestSlot == -1)
+				%bestSlot = -2;
+		}
+	}
 
-    return %bestSlot;
+	return %bestSlot;
 }
 
 function SimpleMagWeaponProps::onRemove(%this)
 {
-    if (isObject(%this.magazine))
-        %this.magazine.delete();
+	if (isObject(%this.magazine))
+		%this.magazine.delete();
 }
 
 function MagazineProps::onAdd(%this)
 {
-    %data = %this.sourceItemData;
+	%data = %this.sourceItemData;
 
-    %this.capacity = %data.magSize;
-    %this.count = %data.magSize;
-    %this.weapon = %data.magWeapon;
-    %this.type = %data.magType;
+	%this.capacity = %data.magSize;
+	%this.count = %data.magSize;
+	%this.weapon = %data.magWeapon;
+	%this.type = %data.magType;
 
-    %this.name = %data.cartName;
-    %this.weight = %data.cartWeight;
+	%this.name = %data.cartName;
+	%this.weight = %data.cartWeight;
 }
 
 datablock ShapeBaseImageData(MagazineImage)
 {
-    shapeFile = "base/data/shapes/empty.dts";
+	shapeFile = "base/data/shapes/empty.dts";
 };
 
 function Player::displayMagazineStats(%this)
 {
-    if (%this.getMountedImage(0) == MagazineImage.getID() && isObject(%this.client))
-    {
-        %props = %this.getItemProps();
-        %text = "<just:center><font:palatino linotype:30>\c3" @ %props.capacity @ "-round<font:palatino linotype:24>\c6 " @ %props.type @ "\n";
-        %text = %text @ "\c6Contains <font:palatino linotype:30>\c3" @ %props.count @ "x <font:palatino linotype:24>\c6" @ %props.name @ " (" @ %props.weight @ "g)\n";
-        %text = %text @ "\c6Used with <font:palatino linotype:30>\c3" @ %props.weapon.uiName @ "\n";
-        commandToClient(%this.client, 'CenterPrint', %text, 0);
-    }
+	if (%this.getMountedImage(0) == MagazineImage.getID() && isObject(%this.client))
+	{
+		%props = %this.getItemProps();
+		%text = "<just:center><font:palatino linotype:30>\c3" @ %props.capacity @ "-round<font:palatino linotype:24>\c6 " @ %props.type @ "\n";
+		%text = %text @ "\c6Contains <font:palatino linotype:30>\c3" @ %props.count @ "x <font:palatino linotype:24>\c6" @ %props.name @ " (" @ %props.weight @ "g)\n";
+		%text = %text @ "\c6Used with <font:palatino linotype:30>\c3" @ %props.weapon.uiName @ "\n";
+		commandToClient(%this.client, 'CenterPrint', %text, 0);
+	}
 }
 
 function MagazineImage::onMount(%this, %obj, %slot)
 {
-    %obj.displayMagazineStats();
+	%obj.displayMagazineStats();
 }
 
 function MagazineImage::onUnMount(%this, %obj, %slot)
 {
-    if (isObject(%obj.client))
-        commandToClient(%obj.client, 'ClearCenterPrint');
+	if (isObject(%obj.client))
+		commandToClient(%obj.client, 'ClearCenterPrint');
 }
 
 datablock ItemData(MagazineItem_45ACP_x7)
 {
-    image = MagazineImage;
-    itemPropsClass = "MagazineProps";
-    customPickupAlways = true;
-    customPickupMultiple = true;
-    canDrop = true;
+	image = MagazineImage;
+	itemPropsClass = "MagazineProps";
+	customPickupAlways = true;
+	customPickupMultiple = true;
+	canDrop = true;
 
-    mass = 1;
+	mass = 1;
 	density = 0.2;
 	elasticity = 0.2;
 	friction = 0.6;
 	emap = true;
 
-    shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/colt_magazine.dts";
-    iconName = "./assets/icons/colt_clip";
-    uiName = "A: M1911";
+	shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/colt_magazine.dts";
+	iconName = "./assets/icons/colt_clip";
+	uiName = "A: M1911";
 
-    magSize = 12;
-    magWeapon = Colt1911Item;
-    magType = "extended detachable box magazine";
+	magSize = 12;
+	magWeapon = Colt1911Item;
+	magType = "extended detachable box magazine";
 
-    cartName = ".45 ACP (11.43x23mm)";
-    cartWeight = 15;
+	cartName = ".45 ACP (11.43x23mm)";
+	cartWeight = 15;
 };
 
 datablock ItemData(MagazineItem_M24A1 : MagazineItem_45ACP_x7)
 {
-    shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/m1garand_clip.dts";
-    iconName = "./assets/icons/m1garand_clip";
-    uiName = "A: M24";
+	shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/m1garand_clip.dts";
+	iconName = "./assets/icons/m1garand_clip";
+	uiName = "A: M24";
 
-    magSize = 5;
-    magWeapon = M24RifleItem;
-    magType = "internal magazine";
+	magSize = 5;
+	magWeapon = M24RifleItem;
+	magType = "internal magazine";
 
-    cartName = "7.62x51mm NATO";
-    cartWeight = 10;
+	cartName = "7.62x51mm NATO";
+	cartWeight = 10;
 };
 
 datablock ItemData(MagazineItem_45ACP_x20_SMG : MagazineItem_45ACP_x7)
 {
-    shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/thompson_clip.dts";
-    iconName = "./assets/icons/thompson_clip";
-    uiName = "A: Thompson";
+	shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/thompson_clip.dts";
+	iconName = "./assets/icons/thompson_clip";
+	uiName = "A: Thompson";
 
-    magSize = 50;
-    magWeapon = ThompsonItem;
-    magType = "drum magazine";
+	magSize = 50;
+	magWeapon = ThompsonItem;
+	magType = "drum magazine";
 
-    cartName = ".45 ACP (11.43x23mm)";
-    cartWeight = 15;
+	cartName = ".45 ACP (11.43x23mm)";
+	cartWeight = 15;
 };
 
 datablock ItemData(MagazineItem_3006_x8 : MagazineItem_45ACP_x7)
 {
-    shapeFile = "./assets/shapes/items/m1garand_clip.dts";
-    iconName = "./assets/icons/m1garand_clip";
-    uiName = "A: Garand";
+	shapeFile = "./assets/shapes/items/m1garand_clip.dts";
+	iconName = "./assets/icons/m1garand_clip";
+	uiName = "A: Garand";
 
-    magSize = 8;
-    magWeapon = M1GarandItem;
-    magType = "en-bloc clip";
+	magSize = 8;
+	magWeapon = M1GarandItem;
+	magType = "en-bloc clip";
 
-    cartName = ".30-06 Springfield (7.62x63mm)";
-    cartWeight = 12;
+	cartName = ".30-06 Springfield (7.62x63mm)";
+	cartWeight = 12;
 };
 
 datablock ItemData(MagazineItem_MicroUzi : MagazineItem_45ACP_x7)
 {
-    shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/colt_magazine.dts";
-    uiName = "A: Micro Uzi";
+    shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/uzi_magazine_20x.dts";
+    uiName = "A: Uzi";
 
-    magSize = 32;
+    magSize = 20;
     magWeapon = MicroUziItem;
     magType = "magazine";
 
     cartName = "9×19mm Parabellum";
-    cartWeight = 15;
+    cartWeight = 15; //Todo: check if this weight is correct
+};
+
+
+datablock ItemData(MagazineItem_MicroUziExtended : MagazineItem_45ACP_x7)
+{
+	shapeFile = "Add-Ons/Weapon_Package_Complex/assets/shapes/items/uzi_magazine_32x.dts";
+	uiName = "A: Uzi Extended";
+
+	magSize = 32;
+	magWeapon = MicroUziItem;
+	magType = "magazine";
+
+	cartName = "9×19mm Parabellum";
+	cartWeight = 15; //Todo: check if this weight is correct
 };
 
 package MagazineStatsPackage
 {
-    function serverCmdUseTool(%client, %slot)
-    {
-        Parent::serverCmdUseTool(%client, %slot);
+	function serverCmdUseTool(%client, %slot)
+	{
+		Parent::serverCmdUseTool(%client, %slot);
 
-        if (isObject(%client.player))
-            %client.player.displayMagazineStats();
-    }
+		if (isObject(%client.player))
+			%client.player.displayMagazineStats();
+	}
 };
 
 activatePackage("MagazineStatsPackage");
--- support/imagetrigger.cs
+++ support/imagetrigger.cs
@@ -1,47 +1,66 @@
 function ShapeBaseImageData::onTrigger(%this, %obj, %slot, %trigger, %state)
 {
-    return 0;
+	return 0;
 }
 
 function ShapeBaseImageData::onLight(%this, %obj, %slot)
 {
-    return 0;
+	return 0;
+}
+
+function ShapeBaseImageData::onDrop(%this, %obj, %slot)
+{
+	return 0;
 }
 
 package ImageTriggerPackage
 {
-    function serverCmdLight(%client)
-    {
-        %player = %client.player;
+	function serverCmdLight(%client)
+	{
+		%player = %client.player;
 
-        if (isObject(%player) && !isObject(%player.light))
-        {
-            for (%i = 0; %i < 4; %i++)
-            {
-                %image = %player.getMountedImage(%i);
+		if (isObject(%player) && !isObject(%player.light))
+		{
+			for (%i = 0; %i < 4; %i++)
+			{
+				%image = %player.getMountedImage(%i);
 
-                if (isObject(%image) && %image.onLight(%player, %i))
-                    %stop = 1;
-            }
-        }
+				if (isObject(%image) && %image.onLight(%player, %i))
+					%stop = 1;
+			}
+		}
 
-        if (!%stop)
-            Parent::serverCmdLight(%client);
-    }
+		if (!%stop)
+			Parent::serverCmdLight(%client);
+	}
 
-    function Armor::onTrigger(%this, %obj, %slot, %state)
-    {
-        for (%i = 0; %i < 4; %i++)
-        {
-            %image = %obj.getMountedImage(%i);
+	function Armor::onTrigger(%this, %obj, %slot, %state)
+	{
+		for (%i = 0; %i < 4; %i++)
+		{
+			%image = %obj.getMountedImage(%i);
 
-            if (isObject(%image) && %image.onTrigger(%obj, %i, %slot, %state))
-                %stop = 1;
-        }
+			if (isObject(%image) && %image.onTrigger(%obj, %i, %slot, %state))
+				%stop = 1;
+		}
 
-        if (!%stop)
-            Parent::onTrigger(%this, %obj, %slot, %state);
-    }
+		if (!%stop)
+			Parent::onTrigger(%this, %obj, %slot, %state);
+	}
+
+	function serverCmdDropTool(%client, %slot) {
+		%player = %client.player;
+		if (isObject(%player)) {
+			for (%i = 0; %i < 4; %i++)
+			{
+				%image = %player.getMountedImage(%i);
+				if (isObject(%image) && %player.tool[%slot] == %image.item && %image.onDrop(%obj, %i, %slot))
+					%stop = 1;
+			}
+		}
+		if (!%stop)
+			parent::serverCmdDropTool(%client, %slot);
+	}
 };
 
 activatePackage("ImageTriggerPackage");
